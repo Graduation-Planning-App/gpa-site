df5942de6e4230a31c7aadf1f5c58e8a179675f4
KyleTelnes <59575950+KyleTelnes@users.noreply.github.com>
2023-04-19 12:12:55 -0700

    course find function

---
+++ assets/js/find-courses.js
@@ -0,0 +1,69 @@
+// Gets unfiltered list of courses on page load
+
+
+// Sends GET request to api on search
+const searchForm = document.getElementById('searchForm');
+searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // All variables to turn in
+    const title = document.getElementById('inputTitle');
+    const discipline = document.getElementById('inputDiscipline');
+    const crn = document.getElementById('inputCRN');
+    const instructor = document.getElementById('inputInstructor');
+    const credits = document.getElementById('inputCredits');
+    const term = document.getElementById('inputTerm');
+    const year = document.getElementById('inputYear');
+    const startTime = document.getElementById('inputStartTime');
+    const endTime = document.getElementById('inputEndTime');
+    let attributes = [];
+    // Put selected attributes into an array
+    const checkBoxes = document.getElementsByClassName('form-check-input');
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked) {
+            attributes.push(checkBoxes[i].value);
+        }
+    }
+    const data = {
+        "title":title.value, // keyword will yield more results, full title will narrow search results
+        "discipline":discipline.value, // discipline code
+        "crn":crn.value, // course crn
+        "year":year.value, // year that course is offered
+        "term":term.value, // quarter that course is offered
+        "credits":credits.value, // number of credits
+        "attributes":attributes, // course attributes, will return results that contain any in the array.
+        "start_time":startTime.value, // start and end time, end time doesn't have to exist. Will return results that fall within the time frame.
+        "end_time":endTime.value,
+        "instruct_methods":"", 
+        "instructors":instructor.value
+    };
+
+    return await sendSearch(data);
+});
+
+// Sends request to api
+async function sendSearch(data) {
+    const response = await fetch( "http://localhost:3000/api/courses/search?input=" + encodeURIComponent(JSON.stringify(data)),
+        {
+            method: "GET",
+        }
+    )
+    console.log(await response.json());
+    return response;
+}
+
+// Leftover code from prior page implementation (may still be useful)
+var coll = document.getElementsByClassName('collapsible');
+      var i;
+
+      for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener('click', function () {
+          this.classList.toggle('active');
+          var content = this.nextElementSibling;
+          if (content.style.display === 'block') {
+            content.style.display = 'none';
+          } else {
+            content.style.display = 'block';
+          }
+        });
+      }
