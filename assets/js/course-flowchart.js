49600617648bed64e65fadae372d29a5c793abfb
KyleTelnes <59575950+KyleTelnes@users.noreply.github.com>
2023-04-30 22:44:17 -0700

    closer to adding edges

--- assets/js/course-flowchart.js
+++ assets/js/course-flowchart.js
@@ -29,16 +29,81 @@ function buildCourseGraph(coursePlan) {
     });
 
     // add graph edges
-
-
-    console.log(graph);
+    graph.forEachNode( (node, attributes) => {
+        const prereqsList = getPrereqs(attributes);
+        console.log(prereqsList);
+    });
 }
 
-function findPrereq(courses) {
+// returns a list of prerequisites for a course
+function getPrereqs(attributes) {
     let prereqs = [];
+    if (!attributes.prerequisites) {
+        return;
+    } else {
+        // put prereqs, ANDs, and ORs into an array
+        let possiblePrereqs = attributes.prerequisites.match(/\w\w\w?\w? \d\d\d\d|AND|OR|\(|\)/g);
+        // loop thru array to handle ANDs and ORs
+        while (possiblePrereqs.length > 0) {
+            const currentElement = possiblePrereqs.shift();
+            const nextElement = possiblePrereqs.shift();
 
-
+            if (currentElement === "(") {
+                // remove other parentheses
+                possiblePrereqs.pop();
+                // add next element back to the array
+                possiblePrereqs.unshift(nextElement);
+            } else if (currentElement === "AND") {
+                // handle group
+                if (nextElement === "(") {
+                    possiblePrereqs.unshift("(");
+                    const list = possiblePrereqs.slice(possiblePrereqs.indexOf('('), possiblePrereqs.indexOf(')'));
+                    prereqs.push(chooseFromList(list));
+                    possiblePrereqs = possiblePrereqs.slice(possiblePrereqs.indexOf(')') + 1);
+                } else {
+                    prereqs.push(nextElement);
+                }
+            } else if (currentElement === "OR") {
+                // handle group
+                if (nextElement === "(") {
+                    possiblePrereqs.unshift("(");
+                    const list = possiblePrereqs.slice(possiblePrereqs.indexOf('('), possiblePrereqs.indexOf(')'));
+                    prereqs.push(chooseFromList(list));
+                    possiblePrereqs = possiblePrereqs.slice(possiblePrereqs.indexOf(')') + 1);
+                } else {
+                    break;
+                }
+            }
+            if (!nextElement) {
+                prereqs.push(currentElement);
+            } else if (nextElement === "AND") {
+                prereqs.push(currentElement);
+                const thirdElement = possiblePrereqs.shift();
+                if (thirdElement === "(") {
+                    // put parentheses back into array
+                    possiblePrereqs.unshift("(");
+                    const list = possiblePrereqs.slice(possiblePrereqs.indexOf('('), possiblePrereqs.indexOf(')'));
+                    prereqs.push(chooseFromList(list));
+                    possiblePrereqs = possiblePrereqs.slice(possiblePrereqs.indexOf(')') + 1);
+                } else {
+                    prereqs.push(thirdElement);
+                }
+            } else if (nextElement === "OR") {
+                // TODO: make this part smarter, for now, it will just choose the first option
+                prereqs.push(currentElement);
+                possiblePrereqs.shift();
+            } else {
+                
+            }
+        }
+    }
     return prereqs;
+}
+
+// returns a prereq or list of prereqs from a list bound by parentheses
+function chooseFromList(prereqList) {
+    // TODO: make this function smarter, right now, will only choose the first prereq (or group of prereqs)
+    return prereqList.at(1);
 }
 
 // Gets users course plans on page load and generate flowcharts
