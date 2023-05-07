50498c9e89c9b51271dbd3a165a3f60abdd16aa4
KyleTelnes <59575950+KyleTelnes@users.noreply.github.com>
2023-05-07 00:54:43 -0700

    bug fixes

--- assets/js/web-components/course-info.js
+++ assets/js/web-components/course-info.js
@@ -39,8 +39,8 @@ class CourseInfo extends HTMLElement {
                 @import url("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css");
             </style>
             <div class="row mb-2 mx-5 px-5 py-2 justify-content-between rounded border">
-                <div class="col-7">${this.#info.course_title}</div>
-                <div class="col-4">${this.#info.class_time}</div>
+                <div class="col-7">${this.#info.course_title ? this.#info.course_title : 'N/A'}</div>
+                <div class="col-4">${this.#info.class_time ? this.#info.class_time : 'N/A'}</div>
                 <div class="col-1"></div>
             </div>
             <div id="moreInfo" class="row mx-5 px-5 justify-content-between"></div>
@@ -53,33 +53,33 @@ class CourseInfo extends HTMLElement {
                 <tbody>
                     <tr class="col">
                         <th scope="row" class="col-3">Credits:</th>
-                        <td class="col-3">${this.#info.credits}</td>
+                        <td class="col-3">${this.#info.credits ? this.#info.credits : 'N/A'}</td>
                         <th scope="row" class="col-3">CRN:</th>
-                        <td class="col-3">${this.#info.crn}</td>
+                        <td class="col-3">${this.#info.crn ? this.#info.crn : 'N/A'}</td>
                     </tr>
                     <tr>
                         <th scope="row" class="col-3">Year:</th>
-                        <td class="col-3">${this.#info.year}</td>
+                        <td class="col-3">${this.#info.year ? this.#info.year : 'N/A'}</td>
                         <th scope="row" class="col-3">Term:</th>
-                        <td class="col-3">${this.#info.term}</td>
+                        <td class="col-3">${this.#info.term ? this.#info.term : 'N/A'}</td>
                     </tr>
                     <tr>
                         <th scope="row" class="col-3">Location:</th>
-                        <td class="col-3">${this.#info.location}</td>
+                        <td class="col-3">${this.#info.location ? this.#info.location : 'N/A'}</td>
                         <th scope="row" class="col-3">Instructors:</th>
                         <td class="col-3">${this.#info.instructors ? this.#info.instructors.toString() : 'N/A'}</td>
                     </tr>
                     <tr>
                         <th scope="row" class="col-3">Prerequisites:</th>
-                        <td class="col-3">${this.#info.prerequisites}</td>
+                        <td class="col-3">${this.#info.prerequisites ? this.#info.prerequisites : 'N/A'}</td>
                         <th scope="row" class="col-3">Corequisites:</th>
-                        <td class="col-3">${this.#info.corequisites}</td>
+                        <td class="col-3">${this.#info.corequisites ? this.#info.corequisites : 'N/A'}</td>
                     </tr>
                     <tr>
                         <th scope="row" class="col-3">Attributes:</th>
-                        <td class="col-3">${this.#info.attributes.toString()}</td>
+                        <td class="col-3">${this.#info.attributes.toString() ? this.#info.attributes.toString() : 'N/A'}</td>
                         <th scope="row" class="col-3">Restrictions:</th>
-                        <td class="col-3">${this.#info.restrictions}</td>
+                        <td class="col-3">${this.#info.restrictions ? this.#info.restrictions : 'N/A'}</td>
                     </tr>
                 </tbody>
             </table>
