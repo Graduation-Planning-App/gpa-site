13c925110b5a9ee67147adfd913ede35fd602973
sharpej1 <jsharpe@spu.edu>
2023-05-14 21:00:16 -0700

    continuing to work on the sign up stuff

--- src/profile/signup.js
+++ src/profile/signup.js
@@ -1,34 +1,45 @@
-async signup() {
-	const form = this.shadowRoot.getElementById('signupForm');
-	const formData = new FormData(form);
-	// create request JSON
-	let request = {};
-	request.email = formData.get('email').toString();
-	request.username = formData.get('uname').toString();
-	request.password = formData.get('psw').toString();
-	request.passwordRep = formData.get('psw-repeat').toString();
-
-	const response = await fetch(
-		import.meta.env.VITE_API_BASEURL + "/api/users/register", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			credentials: 'include',
-			mode: "cors",
-			body: JSON.stringify(request)
-		}
-	);
-	console.log(request);
-	if (response.status !== 200) {
-		const error = await response.json();
-		// error message
-		let errorBox = this.shadowRoot.getElementById('error');
-		errorBox.innerHTML = '';
-		errorBox.innerHTML = error.message;
+class SignUpForm {
+	constructor() {
+		super();
+		this.render();
 	}
-	else {
-		// unsure about this code here
-		localStorage.setItem('isRegistered','true');
+	render() {
+		const signupForm = this.shadowRoot.getElementById('signupForm');
+		signupForm.addEventListener("submit", signup);
+	}
+
+	async signup() {
+		const form = this.shadowRoot.getElementById('signupForm');
+		const formData = new FormData(form);
+		// create request JSON
+		let request = {};
+		request.email = formData.get('email').toString();
+		request.username = formData.get('uname').toString();
+		request.password = formData.get('psw').toString();
+		request.passwordRep = formData.get('psw-repeat').toString();
+
+		const response = await fetch(
+			import.meta.env.VITE_API_BASEURL + "/api/users/register", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				credentials: 'include',
+				mode: "cors",
+				body: JSON.stringify(request)
+			}
+		);
+		if (response.status !== 200) {
+			const error = await response.json();
+			// error message
+			let errorBox = this.shadowRoot.getElementById('error');
+			errorBox.innerHTML = '';
+			errorBox.innerHTML = error.message;
+		}
+		else {
+			// unsure about this code here
+			window.location.replace("/");
+		}
+		console.log("test");
 	}
 }
