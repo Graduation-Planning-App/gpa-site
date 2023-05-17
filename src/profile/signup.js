47dc82bece6e7a86bad75653346a27b28d332b53
sharpej1 <jsharpe@spu.edu>
2023-05-16 21:41:21 -0700

    sign up stuff now sends request to backend

--- src/profile/signup.js
+++ src/profile/signup.js
@@ -1,45 +1,42 @@
-class SignUpForm {
-	constructor() {
-		super();
-		this.render();
+async function signup() {
+	const form = document.getElementById('signupForm');
+	const formData = new FormData(form);
+	let errorBox = document.getElementById('error');
+	errorBox.innerHTML = '';
+	// create request JSON
+	let request = {};
+	request.email = formData.get('email').toString();
+	request.name = formData.get('uname').toString();
+	if ((formData.get('psw').toString()).localeCompare(formData.get('psw-repeat').toString()) !== 0) {
+		errorBox.innerHTML = 'Passwords do not match!';
+		return;
 	}
-	render() {
-		const signupForm = this.shadowRoot.getElementById('signupForm');
-		signupForm.addEventListener("submit", signup);
+	request.password = formData.get('psw').toString();
+
+	const response = await fetch(
+		import.meta.env.VITE_API_BASEURL + "/api/users/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			credentials: 'include',
+			mode: "cors",
+			body: JSON.stringify(request)
+		}
+	);
+	if (response.status !== 200) {
+		const error = await response.json();
+		// error message
+		errorBox.innerHTML = error.message;
 	}
-
-	async signup() {
-		const form = this.shadowRoot.getElementById('signupForm');
-		const formData = new FormData(form);
-		// create request JSON
-		let request = {};
-		request.email = formData.get('email').toString();
-		request.username = formData.get('uname').toString();
-		request.password = formData.get('psw').toString();
-		request.passwordRep = formData.get('psw-repeat').toString();
-
-		const response = await fetch(
-			import.meta.env.VITE_API_BASEURL + "/api/users/register", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				credentials: 'include',
-				mode: "cors",
-				body: JSON.stringify(request)
-			}
-		);
-		if (response.status !== 200) {
-			const error = await response.json();
-			// error message
-			let errorBox = this.shadowRoot.getElementById('error');
-			errorBox.innerHTML = '';
-			errorBox.innerHTML = error.message;
-		}
-		else {
-			// unsure about this code here
-			window.location.replace("/");
-		}
-		console.log("test");
+	else {
+		window.location.replace("/");
 	}
 }
+
+const form = document.getElementById("signupForm")
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	await signup();
+	return;
+})
