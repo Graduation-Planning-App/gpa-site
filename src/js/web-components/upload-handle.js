915b5d3b43c0b9174d5bbb7d16db36270c9ffcc4
NathanDoan07 <doann@spu.edu>
2023-06-01 19:12:51 -0700

    Pushing random

--- src/js/web-components/upload-handle.js
+++ src/js/web-components/upload-handle.js
@@ -5,7 +5,7 @@ input.style.opacity = 0;
 const fileInput = document.getElementById('file_input');
 let file = null;
 const form = document.getElementById('uploadForm');
-const submitButton = document.getElementById('submit');
+const submit = document.getElementById('submitBtn');
 var courseList = document.getElementById('course-list');
 
 
@@ -31,21 +31,53 @@ fileInput.addEventListener('change', () => {
         })
         .then(res => res.json())
         .then(data => {
-            
-            var p = document.getElementById('6-p');
             var courselist = document.getElementById('course-list');
-            p.textContent = '';
-
+            /*var p = document.getElementById('6-p');
+            p.textContent = 'After confirming these are your classes, 
+            hit submit to add them to your completed courses! (For now does not support transfer credits)';*/
 
             for (let i = 0; i < data.length; i++) {
                 let li = courselist.appendChild(document.createElement("li"));
                 const innerArray = data[i];
                 const crn = innerArray[4];
                 const name = innerArray[2];
+                li.value = `[Course Name: ${name} CRN: ${crn}]`;
+                //li.textContent = `[Course Name: ${name} CRN: ${crn}]`;
+                /*let li = courselist.appendChild(document.createElement("p"));
+                const innerArray = data[i];
+                const crn = innerArray[4];
+                const name = innerArray[2];
                 li.value = crn, name;
                 li.innerHTML = `[Course Name: ${name} CRN: ${crn}]`;
+                li.style.opacity = 0;*/
             }
             console.log(typeof data, data);
+
+            
+
+            submit.addEventListener('click', () => {
+                /*const liElements = document.querySelectorAll('#course-list li');
+                const selectedCourses = Array.from(liElements).map(li => li.innerHTML);
+                console.log(selectedCourses);*/
+
+                const selectedCourses = data;
+                
+                fetch(import.meta.env.VITE_API_BASEURL + "/api/users/confirmCourse", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(selectedCourses)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('Processed correctly');
+                    })
+                    .catch(err=> {
+                        console.error('Error:', err);
+                    })
+            });
+        
         })
         .catch(err => {
             console.error('Error:', err);
@@ -54,35 +86,3 @@ fileInput.addEventListener('change', () => {
     return;
     }
 });
-
-
-
-/*
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const file = fileInput.files[0];
-    if (file.type !== 'text/html') {
-        fileInput.value = '';
-        preview.textContent = 'Invalid file, please send an HTML file.'
-        return;
-    } else {
-        preview.textContent = `File selected: ${file.name}`;
-
-        const htmlData = new htmlData();
-        htmlData.append('htmlFile', file);
-
-        fetch('/upload', {
-            method: 'POST',
-            body: htmlData
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(err => {
-            console.error('Error:', err);
-        })
-    } 
-});
-*/
